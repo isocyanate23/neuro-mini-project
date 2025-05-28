fcb7bca63ea74dab7fcd6dfb75dee697ac03086b
isocyanate <aakm10304@gmail.com>
2025-05-28 17:52:10 +0530

    Flask Web-App with Object Detection

---
+++ app.py
@@ -0,0 +1,39 @@
+# app.py
+from flask import Flask, Response, render_template
+import cv2
+from ultralytics import YOLO
+
+app = Flask(__name__)
+model = YOLO("yolo-Weights/yolov8n.pt")
+cap = cv2.VideoCapture(0)
+
+def gen_frames():
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            results = model(frame, stream=True)
+            for r in results:
+                for box in r.boxes:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    cls = int(box.cls[0])
+                    label = model.names[cls]
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    cv2.putText(frame, label, (x1, y1 - 10),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
